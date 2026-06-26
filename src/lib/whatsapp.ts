20fc77dd5de7be4148d34de63d74c4b9cc1400eb
Wrappy <marketing.wrapznfryz@gmail.com>
2026-06-26 21:31:01 +0530

    fix: update template language code from en to en_US

--- src/lib/whatsapp.ts
+++ src/lib/whatsapp.ts
@@ -382,7 +382,7 @@ async function sendTemplate(
     type: 'template',
     template: {
       name: templateName,
-      language: { code: 'en' },
+      language: { code: 'en_US' },
       components,
     },
   }
